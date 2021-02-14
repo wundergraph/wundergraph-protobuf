579e6bc8f0b5830220d00f5ba0bb7591c6f8aa45
Jens Neuse <jens.neuse@gmx.de>
2021-02-14 11:38:58 +0100

    fix set npm version

--- ts/setversion.js
+++ ts/setversion.js
@@ -4,8 +4,6 @@ var fs = require("fs");
 console.log("setting version to: " + gitVersion());
 
 const packageJSON = JSON.parse(fs.readFileSync("package.json").toString());
-console.log(packageJSON);
-
 packageJSON.version = gitVersion();
 
 fs.writeFileSync("package.json",JSON.stringify(packageJSON,null,"  "));
