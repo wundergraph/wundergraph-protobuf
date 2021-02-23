74da1f51bf3f02077b412b072dbfc4e496005807
Jens Neuse <jens.neuse@gmx.de>
2021-02-23 08:22:01 +0100

    set version from pipeline only

--- ts/setversion.js
+++ ts/setversion.js
@@ -5,6 +5,5 @@ console.log("setting version to: " + gitVersion());
 
 const packageJSON = JSON.parse(fs.readFileSync("package.json").toString());
 packageJSON.version = gitVersion();
-delete packageJSON.scripts.postinstall;
 
 fs.writeFileSync("package.json",JSON.stringify(packageJSON,null,"  "));
